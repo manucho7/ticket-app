daa93d818290b6f3fd418500c2b112380384b4b9
manucho <manuel.blanco77@gmail.com>
2020-11-26 14:02:46 -0300

    Obteniendo un nuevo ticket via socket emit en <CrearTicket/> func nuevoTicket -- 3rd argument callback gets called by backend

--- src/pages/CrearTicket.js
+++ src/pages/CrearTicket.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Button, Col, Row, Typography } from 'antd';
 import { DownloadOutlined } from '@ant-design/icons';
 import { useHideMenu } from '../hooks/useHideMenu';
+import { SocketContext } from '../context/SocketContext';
 
 
 const { Title, Text } = Typography;
@@ -10,8 +11,17 @@ export const CrearTicket = () => {
 
     useHideMenu(true);
 
+    const { socket } = useContext(SocketContext);
+
+    const [ticket, setTicket] = useState(null);
+
     const nuevoTicket = () => {
-        console.log('nuevo ticket');
+
+        socket.emit('solicitar-ticket', null, (ticket) => {
+            console.log(ticket);
+            setTicket(ticket);
+        });
+
     }
 
     return (
@@ -34,17 +44,22 @@ export const CrearTicket = () => {
                 </Col>
             </Row>
 
-            <Row style={{marginTop: 100}}>
-                <Col span={14} offset={6} align="center">
-                    <Text level={2}>
-                        Su número
-                    </Text>
-                    <br />
-                    <Text type="success" style={{fontSize: 55}}>
-                        55
-                    </Text>
-                </Col>
-            </Row>
+            {
+                ticket && (
+                    <Row style={{marginTop: 100}}>
+                        <Col span={14} offset={6} align="center">
+                            <Text level={2}>
+                                Su número
+                            </Text>
+                            <br />
+                            <Text type="success" style={{fontSize: 55}}>
+                                {ticket.number}
+                            </Text>
+                        </Col>
+                    </Row>
+                )
+            }
+
         </>
     )
 }
